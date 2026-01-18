b8ef03a436fbf3e7164fcbff63e4a355a8b00fcf
CarlosVazPorto <carlosvazporto@gmail.com>
2026-01-18 16:11:42 -0300

    chore: reestruturação do eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -24,6 +24,7 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ])
