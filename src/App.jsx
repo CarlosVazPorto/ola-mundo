2c26a6cdc269fc44fee8b6fb620a7484d1376201
CarlosVazPorto <carlosvazporto@gmail.com>
2026-01-22 17:25:39 -0300

    feat(App.jsx): importa e insere o BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 
-console.log(window.location);
-
-const pagina = window.location.pathname === '/'
-    ? <Inicio />
-    : <SobreMim />;
-
 function App() {
     return (
-        <>
-            {pagina}
-        </>
+        <BrowserRouter>
+        
+        </BrowserRouter>
     )
 }
 
