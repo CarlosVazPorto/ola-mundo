53869674c1829a29777464183f63e53d1299ab31
CarlosVazPorto <carlosvazporto@gmail.com>
2026-02-05 19:10:04 -0300

    feat: mostra texto e imagens no Banner

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -1,8 +1,37 @@
 import styles from './Banner.module.css';
+import circuloColorido from "@/assets/circulo_colorido.png";
+import minhaFoto from "@/assets/minha_foto.png";
 
 const Banner = () => {
     return (
-        "Banner"
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessoal! Eu sou o Carlos Vaz Porto, um desenvolvedor front-end apaixonado por criar experiências digitais incríveis. Aqui compartilho meus projetos, aprendizados e um pouco da minha jornada no mundo do desenvolvimento web. Sinta-se à vontade para explorar e conhecer mais sobre mim e meu trabalho!
+                </p>
+
+            </div>
+
+            <div className={styles.imagens}>
+                <img 
+                    className={styles.circuloColorido} 
+                    src={circuloColorido} 
+                    aria-hidden="true"
+                />
+
+                <img 
+                    className={styles.minhaFoto} 
+                    src={minhaFoto}
+                    alt="Foto do Carlos Vaz Porto"
+                />
+
+            </div>
+
+        </div>
     )
 };
 
