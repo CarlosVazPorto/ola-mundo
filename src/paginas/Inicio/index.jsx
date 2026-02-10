10a7e3c71da7bc56d26ee664c2e4b88cc6d32915
CarlosVazPorto <carlosvazporto@gmail.com>
2026-02-10 11:30:08 -0300

    feat(Inicio/index.jsx): cria estrutura básica para lista de posts

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,12 +1,23 @@
 import Banner from "@/componentes/Banner";
+import styles from "./Inicio.module.css";
+import posts from "@/json/posts.json";
 
 const Inicio = () => {
     return (
-        <main>
-            <Banner />
-    
-            <h1>Olá Mundo!</h1>
-        </main>
+        <>
+            <main>
+                <Banner />
+        
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        
+                    </li>
+                ))}
+            </ul>
+
+            </main>
+        </>
     );
 };
 
