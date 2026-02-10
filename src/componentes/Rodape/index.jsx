c11afd8a6a7c661bd19d1dc73a16ea8e6f64de42
CarlosVazPorto <carlosvazporto@gmail.com>
2026-02-10 12:50:24 -0300

    feat(Rodape/index.jsx): rederiza o rodapé

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -1,10 +1,12 @@
 import styles from "./Rodape.module.css";
-import { ReactComponent as MarcaRegistrada } from "@/assets/imagens/marca_registrada.svg";
+import MarcaRegistrada from '@/assets/imagens/marca_registrada.svg?react'
 
 const Rodape = () => {
     return (
         <footer className={styles.rodape}>
             <MarcaRegistrada />
+
+            Desenvolvido por Alura.
         </footer>
     );
 };
