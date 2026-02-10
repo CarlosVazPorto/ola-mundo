b6845e5bc438d8f648a640f566f546a693864b39
CarlosVazPorto <carlosvazporto@gmail.com>
2026-02-10 12:07:57 -0300

    feat(componentes/Post): renderiza botão Ler do post

--- src/componentes/Post/index.jsx
+++ src/componentes/Post/index.jsx
@@ -12,7 +12,7 @@ const Post = ({ post }) => {
 
             <h2 className={styles.titulo}>{post.titulo}</h2>
 
-            
+            <button className={styles.botaoLer}>Ler</button>
 
         </div>
     )
