75aff629884036f56eb5b22d92f52bf52e74b527
CarlosVazPorto <carlosvazporto@gmail.com>
2026-02-20 17:55:25 -0300

    feat(paginas/SobreMim): renderiza a foto da capa e o título

--- src/paginas/SobreMim/index.jsx
+++ src/paginas/SobreMim/index.jsx
@@ -1,7 +1,15 @@
+import PostModelo from "@/componentes/PostModelo";
+import fotoCapa from '@/assets/imagens/sobre_mim_capa.png';
+
 const SobreMim = () => {
     return (
 
-        <h1>Sobre mim...</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+            conteúdo...
+        </PostModelo>
 
     );
 };
