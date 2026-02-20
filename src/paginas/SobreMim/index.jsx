905c2e5f50bd2a1e20d796148c30e927fd020e9e
CarlosVazPorto <carlosvazporto@gmail.com>
2026-02-20 18:13:27 -0300

    feat(SobreMim/index.jsx): inclusão da foto

--- src/paginas/SobreMim/index.jsx
+++ src/paginas/SobreMim/index.jsx
@@ -1,6 +1,7 @@
+import styles from './SobreMim.module.css';
 import PostModelo from "@/componentes/PostModelo";
 import fotoCapa from '@/assets/imagens/sobre_mim_capa.png';
-import styles from './SobreMim.module.css';
+import fotoSobreMim from '@/assets/imagens/minha_foto_sobre_mim.png';
 
 const SobreMim = () => {
     return (
@@ -10,8 +11,14 @@ const SobreMim = () => {
             titulo="Sobre mim"
         >
             <h3 className={styles.subtitulo}>
-
+                Olá, eu sou o Carlos!
             </h3>
+
+            <img 
+                className={styles.fotoSobreMim}
+                src={fotoSobreMim} 
+                alt="Foto do Carlos" 
+            />
         </PostModelo>
 
     );
