cdad0d5bb45f78c24ab12094794828b6e1606e26
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-05 16:46:24 -0300

    feat(NaoEncontrada/index.jsx): aprimora a estrutura a página NaoEncontrada

--- src/paginas/NaoEncontrada/indesx.jsx
+++ src/paginas/NaoEncontrada/indesx.jsx
@@ -1,33 +1,38 @@
 import styles from './NaoEncontrada.module.css';
-import erro404 from '@/assets/erro404.png';
+import erro404 from '@/assets/imagens/erro_404.png';
 
 const NaoEncontrada = () => {
     return (
-        <div className={styles.conteudoContainer}> 
-            <span className={styles.texto404}>404</span>
+        <>
+            <div className={styles.conteudoContainer}> 
+                <span className={styles.texto404}>404</span>
 
-            <h1 className={styles.titulo}>
-                Ops! Página não encontrada.
-            </h1>
+                <h1 className={styles.titulo}>
+                    Ops! Página não encontrada.
+                </h1>
 
-            <p className={styles.paragrafo}>
-                Tem certeza de que era isso o que você estava procurando?
-            </p>
-            <p className={styles.paragrafo}>
-                Aguarde uns instantes e recarregue a página ou volte para a página inicial.
-            </p>
+                <p className={styles.paragrafo}>
+                    Tem certeza de que era isso o que você estava procurando?
+                </p>
+                <p className={styles.paragrafo}>
+                    Aguarde uns instantes e recarregue a página ou volte para a página inicial.
+                </p>
 
-            <div className={styles.botaoContainer}>
-                <button>Voltar</button>
+                <div className={styles.botaoContainer}>
+                    <button>Voltar</button>
+                </div>
+
+                <img 
+                    className={styles.imagemCachorro}
+                    src={erro404}
+                    alt="Imagem de um cachorro de óculos e vestido como um humano"
+                />
+
             </div>
 
-            <img 
-                className={styles.imagemCachorro}
-                src={erro404}
-                alt="Imagem de um cachorro de óculos e vestido como um humano"
-            />
+            <div className={styles.espacoEmBranco}></div>
 
-        </div>
+        </>
     );
 };
 
