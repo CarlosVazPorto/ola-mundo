eae6cfdae66a565f488763c8eec0c96bd50d135a
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-05 16:19:02 -0300

    feat(NaoEncontrada/index.jsx): estrutura inicialmente a página NaoEncontrada

---
+++ src/paginas/NaoEncontrada/indesx.jsx
@@ -0,0 +1,11 @@
+import styles from './NaoEncontrada.module.css';
+
+const NaoEncontrada = () => {
+    return (
+        <div className={styles.NaoEncontrada}> 
+
+        </div>
+    );
+};
+
+export default NaoEncontrada;
